1955d1d9740230b73c75956d9c699a767c2301df
aldricalsals <alifvianaviantikaa@gmail.com>
2026-06-20 19:45:12 +0700

    Menambahkan API transaksi dan history

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,8 +30,11 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
 
 $routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
 $routes->get('ajax/destinations','TransaksiController::destinations', ['filter' => 'auth']);
+$routes->get('history', 'TransaksiController::history', ['filter' => 'auth']);
+$routes->resource('api/products', ['controller' => 'Api\ProdukController']);
 $routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
 $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
+$routes->get('api/transactions', 'Api\TransaksiController::index');
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -25,4 +25,11 @@
         </li><!-- End Produk Nav --> 
     </ul>
 
+    <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+            <i class="bi bi-person"></i>
+            <span>History</span>
+        </a>
+    </li><!-- End History Nav -->
+
 </aside><!-- End Sidebar-->
